829fa69e8f95799425f86c257a8023b88c3f5afd
jdbanuel <jamesdb1995@gmail.com>
2020-08-31 23:43:56 -0700

    Change url to interpolated string

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,11 +2,14 @@ const apiKey = process.env.REACT_APP_API_TOKEN;
 
 const Yelp = {
 	search(term, location, sortBy) {
-		return fetch(process.env.REACT_APP_API_URL, {
-			headers: {
-				Authorization: `Bearer ${apiKey}`,
-			},
-		})
+		return fetch(
+			`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			}
+		)
 			.then((response) => {
 				return response.json();
 			})
