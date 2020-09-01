f6bb5a1f689b53c149f813760a4ad2a25b01dd3e
jdbanuel <jamesdb1995@gmail.com>
2020-08-31 22:54:04 -0700

    Fix warning

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -78,7 +78,7 @@ class SearchBar extends React.Component {
 					<input onChange={this.handleLocationChange} placeholder="Where?" />
 				</div>
 				<div onClick={this.handleSearch} className="SearchBar-submit">
-					<a>Let's Go</a>
+					<a href="/">Let's Go</a>
 				</div>
 			</div>
 		);
