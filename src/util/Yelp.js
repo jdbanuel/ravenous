e68489d7822ce81c4b11204c7f99e5172f500b2f
jdbanuel <jamesdb1995@gmail.com>
2020-08-31 22:48:01 -0700

    Test for netlify build

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,17 +1,12 @@
-import { apiCredentials } from './credentials';
-
-const apiKey = apiCredentials.key;
+const apiKey = process.env.API_TOKEN;
 
 const Yelp = {
 	search(term, location, sortBy) {
-		return fetch(
-			`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-			{
-				headers: {
-					Authorization: `Bearer ${apiKey}`,
-				},
-			}
-		)
+		return fetch(process.env.API_URL, {
+			headers: {
+				Authorization: `Bearer ${apiKey}`,
+			},
+		})
 			.then((response) => {
 				return response.json();
 			})
